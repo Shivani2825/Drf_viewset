51948a4fececeb3a13d6eb4c0d470e9b72d49fdc
Shivani2825 <jainshivani285@gmail.com>
2023-03-20 11:32:39 +0530

    viewset

--- viewset/viewset/product/views.py
+++ viewset/viewset/product/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets , status
+from rest_framework.response import Response
 from . models import *
 from . serializers import *
 
@@ -7,6 +8,28 @@ from . serializers import *
 class CategoryViewset(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+class CategoryViewset(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({'message': 'Product deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
+
+class CategoryViewset(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
 
 
 class ItemViewset(viewsets.ModelViewSet):
